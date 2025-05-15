367c64f9bde5ed3f328c6004bdeaa14a3525bc50
ryanhva <ryan.yeung@hva.nl>
2025-05-15 16:02:36 +0200

    last

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const xss = require ('xss')
+const html = xss('<script>alert("xss");</script>');
+console.log(html);
+const bcrypt = require('bcryptjs');
+
+
 
 app.use(express.urlencoded({ extended: true }))
 
@@ -12,11 +18,25 @@ app
  .set('views', 'view')
 
  .get('/songList', song)
-
  .get('/', onhome)
  .get('/about', onabout)
+ .get('/formulier', toonformulier)
 
  .listen(8000)
+
+app
+.post('/form', verwerkformulier)
+
+function toonformulier(req, res){
+    res.render('form.ejs')
+}
+function verwerkformulier(req, res) {
+    
+    db.collection('users')
+    .insertOne(req.body)
+    res.send('test')
+    console.log(req.body)
+}
 
 function onhome(req, res) {
     res.send('<h1>Hello World!</h1> <img src="/static/images/snoopy.jpg" alt="Poster" width="50%"/>')
@@ -34,6 +54,30 @@ function song(req, res, ) {
     }
     
     res.render('detail.ejs', {data: song})
+}
+
+// Password hashing and salting with bcrypt
+async function hashData(data){
+  try {
+    const salt = await bcrypt.genSalt(10)
+    const hashedData = await bcrypt.hash(data, salt)
+    return hashedData
+  }
+  catch(error) {
+    console.log("Error hashing password: ", error)
+  }
+}
+
+// Compare given and stored data
+async function compareData(plainTextData, hashedData) {
+  try {
+      // Compare the plain text data with the hashed data
+      const match = await bcrypt.compare(plainTextData, hashedData);
+      return match;
+  } catch (error) {
+      console.error('Error comparing data:', error);
+      throw error;
+  }
 }
 
 
